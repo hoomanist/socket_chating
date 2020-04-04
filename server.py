ef20ac7d48e7b1d266d0f9ccef03d153c3cffffd
hooman hedayti <hham.group12@gmail.com>
2020-04-04 20:15:54 +0430

    an error fixed

--- server.py
+++ server.py
@@ -4,10 +4,10 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(("0.0.0.0", 8001))
 serversocket.listen(1)
 conn, addr = serversocket.accept()
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
+with conn:
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
 printer = f"{addr[0]} said {data.decode()}"
 print(printer)
